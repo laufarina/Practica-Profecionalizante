187bd836d24092d3dc24e1d611a4a31e14c85352
laufarina <144963168+laufarina@users.noreply.github.com>
2024-08-25 18:43:06 -0300

    Add files via upload

---
+++ ProyectoInscripcionesPolis/assets/js/main1.js
@@ -0,0 +1,11 @@
+// Selecciona el elemento select por su ID
+const distrito = document.getElementById('distrito');
+
+// Establece el texto del placeholder
+distrito.options[0].text = 'Seleccione su localidad.';
+
+// Selecciona el elemento select por su ID
+const actividad = document.getElementById('actividad');
+
+// Establece el texto del placeholder
+acticidad.options[0].text = 'Seleccione la actividad de su preferencia.';
